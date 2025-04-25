bd7ff71ec389b5fa71c27902c99f5be9b70eeb5a
Student <student@mx23-hp>
2025-04-25 16:31:15 +0100

    ReddaprogramB

---
+++ quadb.go
@@ -0,0 +1,36 @@
+package quad
+
+import "fmt"
+
+func QuadB(x, y int) {
+    if x <= 0 || y <= 0 {
+        return
+    }
+    
+    for i := 0; i < y; i++ {
+        for j := 0; j < x; j++ {
+            if i == 0 || i == y-1 {
+                if j == 0 || j == x-1 {
+                    fmt.Print("+")
+                } else {
+                    fmt.Print("-")
+                }
+            } else {
+                if j == 0 || j == x-1 {
+                    fmt.Print("|")
+                } else {
+                    fmt.Print(" ")
+                }
+            }
+        }
+        fmt.Println()
+    }
+}
+
+package quad
+
+import "piscine"
+
+func main() {
+    piscine.QuadB(5, 3)
+}
