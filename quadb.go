763120a8ddf9e20e8e80736d386ef284df523fcd
gappeagy <appeagyeigeorgina@gmail.com>
2025-04-25 18:29:40 +0100

    quad group project

--- quadb.go
+++ quadb.go
@@ -1,32 +1,35 @@
 package quad
 
-import "fmt"
+// import (
+// 	"fmt"
+// 	"quad"
+// )
 
-func QuadB(x, y int) {
-    if x <= 0 || y <= 0 {
-        return
-    }
+// func QuadB(x, y int) {
+//     if x <= 0 || y <= 0 {
+//         return
+//     }
     
-    for i := 0; i < y; i++ {
-        for j := 0; j < x; j++ {
-            if i == 0 || i == y-1 {
-                if j == 0 || j == x-1 {
-                    fmt.Print("+")
-                } else {
-                    fmt.Print("-")
-                }
-            } else {
-                if j == 0 || j == x-1 {
-                    fmt.Print("|")
-                } else {
-                    fmt.Print(" ")
-                }
-            }
-        }
-        fmt.Println()
-    }
-}
+//     for i := 0; i < y; i++ {
+//         for j := 0; j < x; j++ {
+//             if i == 0 || i == y-1 {
+//                 if j == 0 || j == x-1 {
+//                     fmt.Print("+")
+//                 } else {
+//                     fmt.Print("-")
+//                 }
+//             } else {
+//                 if j == 0 || j == x-1 {
+//                     fmt.Print("|")
+//                 } else {
+//                     fmt.Print(" ")
+//                 }
+//             }
+//         }
+//         fmt.Println()
+//     }
+// }
 
-func main() {
-    piscine.QuadB(5, 3)
-}
+// func main() {
+//     quad.QuadB(5, 3)
+// }
--- quadc.go
+++ quadc.go
@@ -5,9 +5,19 @@ import (
 )
 
 func QuadC(x,y int) {
-	if (x > 0 && y > 0) {
-		for i : 0; i < x; i++ {
-			fmt.Print("B")
+	if x > 0 && y > 0 {
+		for i := 0; i < x; i++ {
+			if i == 0 || i == x-1 {
+				fmt.Print("A")
+			} else {
+				fmt.Print("B")
+			}
+			if(i > 0 && i < x-1) {
+				fmt.Print("\n")
+				for j := 0; j < y; j++ {
+					fmt.Print("B")
+				}
+			}
 		}
 	}
 }
