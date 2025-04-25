aaf5fcb70832e87031451aee2f55b10cf473f267
gappeagy <appeagyeigeorgina@gmail.com>
2025-04-25 16:49:14 +0100

    re-add Redda's work and remove duplicate imports and packages

---
+++ quadb.go
@@ -0,0 +1,32 @@
+package quad
+
+import "fmt"
+
+func QuadB(x, y int) {
+    if x <= 0 || y <= 0 {
+        return
+    }
+    
+    for i := 0; i < y; i++ {
+        for j := 0; j < x; j++ {
+            if i == 0 || i == y-1 {
+                if j == 0 || j == x-1 {
+                    fmt.Print("+")
+                } else {
+                    fmt.Print("-")
+                }
+            } else {
+                if j == 0 || j == x-1 {
+                    fmt.Print("|")
+                } else {
+                    fmt.Print(" ")
+                }
+            }
+        }
+        fmt.Println()
+    }
+}
+
+func main() {
+    piscine.QuadB(5, 3)
+}
