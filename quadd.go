5560b42d8b83caf405c62128bb6b936bb294e303
Student <student@mx23-hp>
2025-04-27 19:56:52 +0100

    file

--- quadd.go
+++ quadd.go
@@ -8,8 +8,10 @@ func QuadD(x, y int) {
 	}
 	for row := 1; row <= y; row++ {
 		for col := 1; col <= x; col++ {
-			if (row == 1 || row == y) && (col == 1 || col == x) {
+			if (row == 1 || row == y) && (col == 1) {
 				fmt.Print("A")
+			} else if (row == 1 || row == y) && (col == x) {
+				fmt.Print("C")
 			} else if row == 1 || row == y {
 				fmt.Print("B")
 			} else if col == 1 || col == x {
