7b06cae5199775a339f7761fb790c19bcece6d3b
msaed <masoudsaed918@gmail.com>
2025-04-27 17:12:15 +0100

    final change have been made

--- quadb.go
+++ quadb.go
@@ -1,37 +1,37 @@
 package quad
 
-// import (
-// 	"fmt"
-// 	"quad"
-// )
+import "fmt"
 
-// func QuadB(x, y int) {
-//     if x <= 0 || y <= 0 {
-//         return
-//     }
-    
-//     for i := 0; i < y; i++ {
-//         for j := 0; j < x; j++ {
-//             if i == 0 || i == y-1 {
-//                 if j == 0 || j == x-1 {
-//                     fmt.Print("+")
-//                 } else {
-//                     fmt.Print("-")
-//                 }
-//             } else {
-//                 if j == 0 || j == x-1 {
-//                     fmt.Print("|")
-//                 } else {
-//                     fmt.Print(" ")
-//                 }
-//             }
-//         }
-//         fmt.Println()
-//     }
-// }
+func QuadB(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
 
-
-// func main() {
-//     quad.QuadB(5, 3)
-// }
-
+	for row := 1; row <= y; row++ {
+		for col := 1; col <= x; col++ {
+			if row == 1 && col == 1 {
+				// Top-left corner
+				fmt.Print("/")
+			} else if row == 1 && col == x {
+				// Top-right corner
+				fmt.Print("\\")
+			} else if row == y && col == 1 {
+				// Bottom-left corner
+				fmt.Print("\\")
+			} else if row == y && col == x {
+				// Bottom-right corner
+				fmt.Print("/")
+			} else if row == 1 || row == y {
+				// Top and bottom borders
+				fmt.Print("*")
+			} else if col == 1 || col == x {
+				// Left and right borders
+				fmt.Print("*")
+			} else {
+				// Inside
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
